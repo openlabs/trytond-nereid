7999d28e61f2890586b9c40bb46e042e862795e1
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2013-01-18 12:27:14 +0530

    Bump to development version 2.4.0.8dev

--- __tryton__.py
+++ __tryton__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Nereid',
-    'version': '2.4.0.7',
+    'version': '2.4.0.8dev',
     'author': 'Openlabs Technologies & Consulting (P) Limited',
     'email': 'info@openlabs.co.in',
     'website': 'http://www.openlabs.co.in/',
